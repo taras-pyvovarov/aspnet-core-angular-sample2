2adaa55222a599a528090342f89f1c4a604e2c93
Taras Pyvovarov <taras.pyvovarov@globallogic.com>
2017-03-14 17:34:27 +0200

    Added dotnet sample

--- build/build.fsx
+++ build/build.fsx
@@ -1,45 +1,29 @@
 #r "./packages/fake/tools/FakeLib.dll" 
-open System
-open Fake
-open System.IO
-open Microsoft.FSharp.Reflection
-open FSharp.Data
 
-//Welcome FAKE build text
+open Fake
+
+
 let buildHeaderText = """
 ***************************************
 ***********FAKE BUILD SAMPLE***********
 ***************************************
 """
 
-Target "ShowHeader" (fun _ ->
+let dotnetVersion = DotNetCli.getVersion ()
+
+//Welcome text, build init messages
+Target "ShowInitMessage" (fun _ ->
     printfn "%s" buildHeaderText
+    printfn "dotnet CLI version: %s" dotnetVersion
 )
 
 Target "Build" (fun _ ->
-    printfn "%s" "Hello"
+    trace "hello"
 )
 
-
-// Target "Build" (fun _ ->
-//     let result = ExecProcessAndReturnMessages(fun p ->
-//                 p.FileName <- "powershell.exe"
-//                 //p.Arguments <- commandFormatString)
-//                 (TimeSpan.FromMinutes 5.0)
-//     //let result = ExecProcess (fun info -> 
-//       //  info.FileName <- "c:/MyProc.exe" 
-//         //info.WorkingDirectory <- "c:/workingDirectory" 
-//         //info.Arguments <- "-v"
-//     //) (TimeSpan.FromMinutes 5.0)
-
-//     if result <> 0 then 
-//         failwithf "MyProc.exe returned with a non-zero exit code"
-//         //"dotnet --help"
-// )
-
-"ShowHeader"
+"ShowInitMessage"
 ==> "Build"
 
 
 
-RunTargetOrDefault "Build"
+RunTargetOrDefault "ShowInitMessage"
