a9ec384c27d50ce77fd73036591e4759e421c1b1
Taras Pyvovarov <taras.pyvovarov@globallogic.com>
2017-03-14 18:05:51 +0200

    Added dotnet restore

--- build/build.fsx
+++ build/build.fsx
@@ -9,7 +9,18 @@ let buildHeaderText = """
 ***************************************
 """
 
+//---------------------Working with dotnet CLI---------------------
+//http://fsharp.github.io/FAKE/apidocs/fake-dotnetcli.html
+
 let dotnetVersion = DotNetCli.getVersion ()
+
+let dotnetPublish = (fun () ->
+    DotNetCli.Restore (fun p -> 
+    { p with 
+        NoCache = true;
+        Project = @"..\src\aspnet-core-angular-sample2\aspnet-core-angular-sample2.csproj";
+    })
+)
 
 //Welcome text, build init messages
 Target "ShowInitMessage" (fun _ ->
@@ -18,7 +29,7 @@ Target "ShowInitMessage" (fun _ ->
 )
 
 Target "Build" (fun _ ->
-    trace "hello"
+    dotnetPublish()
 )
 
 "ShowInitMessage"
@@ -26,4 +37,4 @@ Target "Build" (fun _ ->
 
 
 
-RunTargetOrDefault "ShowInitMessage"
+RunTargetOrDefault "Build"
