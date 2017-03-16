49967379d79ddaf1365f449945405f2126f797b3
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2017-03-17 01:04:42 +0200

    Moved from attribute to template routing

--- src/aspnet-core-angular-sample2/Controllers/HomeController.cs
+++ src/aspnet-core-angular-sample2/Controllers/HomeController.cs
@@ -2,12 +2,8 @@
 
 namespace aspnet_core_angular_sample2.Controllers
 {
-    [Route("")]
-    [Route("Home")]
     public class HomeController : Controller
     {
-        [Route("")]
-        [Route(nameof(Index))]
         public IActionResult Index()
         {
             return View();
@@ -17,7 +13,6 @@ namespace aspnet_core_angular_sample2.Controllers
         /// Redirect here if exception occurred in code due to bad input request or code bug.
         /// Will hit if any crash in .net code occurs.
         /// </summary>
-        [Route(nameof(Error))]
         public IActionResult Error()
         {
             //Maybe log something.
