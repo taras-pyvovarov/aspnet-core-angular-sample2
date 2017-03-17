68fcbd0c0fe8884f88acccc4927230a9363d0e3c
Taras Pyvovarov <taras.pyvovarov@globallogic.com>
2017-03-17 18:17:26 +0200

    Added calc logics

--- src/aspnet-core-angular-sample2/SampleApp1/components/calculator-component/calculator-component.ts
+++ src/aspnet-core-angular-sample2/SampleApp1/components/calculator-component/calculator-component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    //!!!
     selector: 'calculator',
-
-    //!!!
     template: require('./calculator-component.html'),
 })
 
-//!!!
-export class CalculatorComponent { }
+export class CalculatorComponent 
+{
+    var1: string = 'Simplest Angular2 binding';
+
+    func1()
+    {
+        let a1: string = 'Hello';
+        let a2: string = 'from';
+        let a3: string = 'function';
+
+        return `${a1} ${a2} ${a3}!`;
+    }
+}
