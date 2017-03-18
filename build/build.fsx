4d3fdc555de10710250e265c3927a25ebfb24164
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2017-03-19 00:26:57 +0200

    Building cmplete package

--- build/build.fsx
+++ build/build.fsx
@@ -1,21 +1,26 @@
 #r "./packages/fake/tools/FakeLib.dll" 
 open System.IO
 open Fake
+open Fake.NpmHelper
 
 //Working with FileSystemHelper:
 //http://fsharp.github.io/FAKE/apidocs/fake-filesystemhelper.html
 //Working with DotNetCli:
 //http://fsharp.github.io/FAKE/apidocs/fake-dotnetcli.html
+//Working with NpmHelper:
+//http://fsharp.github.io/FAKE/apidocs/fake-npmhelper.html
+//Working with FileHelper:
+//http://fsharp.github.io/FAKE/apidocs/fake-filehelper.html
 
 let buildHeaderText = """
-***************************************
-***********FAKE BUILD SAMPLE***********
-***************************************
+---------------------------------------------------------
+********************FAKE BUILD SAMPLE********************
+---------------------------------------------------------
 """
 
 let currentDir = FileSystemHelper.currentDirectory
 let projectRootDir = Directory.GetParent(currentDir).FullName;
-let dotnetPublishDir = projectRootDir @@ @"buildArtifact"
+let dotnetPublishDir = projectRootDir @@ "buildArtifact"
 
 
 let dotnetVersion = DotNetCli.getVersion ()
@@ -38,6 +43,35 @@ let dotnetPublish = (fun () ->
     })
 )
 
+let npmInstall = (fun () ->
+    Npm (fun p ->
+    { p with
+        Command = Install Standard
+        WorkingDirectory = @"..\src\aspnet-core-angular-sample2\"
+    })
+)
+
+let webpackBuild = (fun () ->
+    //Running vendor webpack flow.
+    let command = projectRootDir @@ @"src\aspnet-core-angular-sample2\node_modules\.bin\webpack.cmd"
+    let args = @"--config webpack.config.vendor.js"
+    let workingDir = @"..\src\aspnet-core-angular-sample2\"
+    //printfn "Command: %s %s" command args
+    let result = Shell.Exec(command, args, workingDir)
+
+    //Running app webpack flow.
+    let command = projectRootDir @@ @"src\aspnet-core-angular-sample2\node_modules\.bin\webpack.cmd"
+    let args = @"--config webpack.config.js"
+    let workingDir = @"..\src\aspnet-core-angular-sample2\"
+    //printfn "Command: %s %s" command args
+    let result = Shell.Exec(command, args, workingDir)
+)
+
+let copyNodeModules = (fun () ->
+    FileHelper.CopyDir (dotnetPublishDir @@ @"node_modules") (projectRootDir @@ @"src\aspnet-core-angular-sample2\node_modules") allFiles
+)
+
+
 //Welcome text, build init messages
 Target "ShowInitMessage" (fun _ ->
     printfn "%s" buildHeaderText
@@ -46,13 +80,28 @@ Target "ShowInitMessage" (fun _ ->
     printfn "current directory: %s" currentDir
 )
 
+Target "NpmInstall" (fun _ ->
+    npmInstall()
+)
+
+Target "WebpackBuild" (fun _ ->
+    webpackBuild()
+)
+
 Target "Build" (fun _ ->
     dotnetPublish()
 )
 
+Target "CopyNodeModules" (fun _ ->
+    copyNodeModules()
+)
+
+
 "ShowInitMessage"
+==> "NpmInstall"
+==> "WebpackBuild"
 ==> "Build"
+==> "CopyNodeModules"
 
 
-
-RunTargetOrDefault "Build"
+RunTargetOrDefault "CopyNodeModules"
